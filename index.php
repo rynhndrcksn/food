06679df3302b709d6a10ccbc3b746c210ea55ba0
Ryan H <rhendrickson11@mail.greenriver.edu>
2021-01-26 15:54:55 -0500

    added breakfast, lunch, and sandwich and their respective images

--- index.php
+++ index.php
@@ -18,10 +18,64 @@ $f3->route('GET /', function() {
 	echo $view->render('views/home.html');
 });
 
+// define a default route (home page) again that takes 2 parameters for some reason?
+$f3->route('GET /@first/$last', function($f3, $params) {
+	echo 'Hello, '.$params['first'].' '.$params['last'];
+});
+
 // define a breakfast route
 $f3->route('GET /breakfast', function() {
 	// create a new view and send it to the client
-	echo "Breakfast";
+	$view = new Template();
+	echo $view->render('views/breakfast.html');
+});
+
+// define a breakfast route with a custom item???
+$f3->route('GET /breakfast/@item', function($f3, $params) {
+	// create a new view and send it to the client
+	echo '<pre>';
+	var_dump($params);
+	echo '</pre>';
+
+	$menu = array('eggs', 'waffles', 'pancakes');
+	$item = $params['item'];
+
+	if (in_array($item, $menu)) {
+		switch ($item) {
+			case 'eggs':
+				$view = new Template();
+				echo $view->render('views/eggs.html');
+				break;
+			case 'waffles':
+				echo "Swedish or American?";
+				break;
+			case 'pancakes':
+				$f3->reroute('https://www.thewafflehouse.com');
+				break;
+			default:
+				$f3->error(404);
+		}
+		echo "Yes, we serve $item.";
+	} else {
+		echo "Sorry, we don't serve $item.";
+	}
+
+	//$view = new Template();
+	//echo $view->render('views/breakfast.html');
+});
+
+// define a lunch route
+$f3->route('GET /lunch', function() {
+	// create a new view and send it to the client
+	$view = new Template();
+	echo $view->render('views/lunch.html');
+});
+
+// define a lunch/sandwich route
+$f3->route('GET /lunch/sandwich', function() {
+	// create a new view and send it to the client
+	$view = new Template();
+	echo $view->render('views/sandwich.html');
 });
 
 // run fat free HAS TO BE THE LAST THING IN FILE
